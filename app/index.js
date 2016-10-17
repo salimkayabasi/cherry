fa5140b7bea52b4c94469d6f2f1d41ed39ca14b8
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-10-17 20:50:05 +0300

    adding express fw for better http handling

--- app/index.js
+++ app/index.js
@@ -1,3 +1,29 @@
-module.exports = exports = function () {
+var async = require('async');
+var config = require('config');
+var log = require('kapgel-logger')(module);
 
+module.exports = exports = function (callback) {
+    var app = require('express')();
+    async.seq(
+        function (cb) {
+            cb(null, app);
+        }
+    )(function (err, server) {
+        if (err) {
+            throw err;
+        }
+        if (callback) {
+            log.info('app started for testing');
+            callback(app);
+        } else {
+            var port = config.server.port;
+            server.listen(port, function (error) {
+                if (error) {
+                    throw error;
+                }
+                log.info('app server listening on port', port);
+                log.info('app status', config.status);
+            });
+        }
+    });
 };
