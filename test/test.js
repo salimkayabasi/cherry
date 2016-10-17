e5fb37224a3a0c4f57eca4bd756ad6b65412d941
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-10-17 21:01:15 +0300

    example test codes

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var dirtyChai = require('dirty-chai');
+chai.use(dirtyChai);
+var expect = chai.expect;
+
+describe('Just adding a basic test example', function () {
+    describe('2 plus 2', function () {
+        it('should be 4', function () {
+            expect(2 + 2).to.be.equal(4);
+        })
+    })
+});
