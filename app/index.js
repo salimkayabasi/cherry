4dbe2fdc643bb60aa018306213d1291f55ee68aa
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2016-10-17 21:23:26 +0300

    optimizing js files according to eslint rules

--- app/index.js
+++ app/index.js
@@ -3,27 +3,27 @@ var config = require('config');
 var log = require('kapgel-logger')(module);
 
 module.exports = exports = function (callback) {
-    var app = require('express')();
-    async.seq(
-        function (cb) {
-            cb(null, app);
+  var app = require('express')();
+  async.seq(
+    function (cb) {
+      cb(null, app);
+    }
+  )(function (err, server) {
+    if (err) {
+      throw err;
+    }
+    if (callback) {
+      log.info('app started for testing');
+      callback(app);
+    } else {
+      var port = config.server.port;
+      server.listen(port, function (error) {
+        if (error) {
+          throw error;
         }
-    )(function (err, server) {
-        if (err) {
-            throw err;
-        }
-        if (callback) {
-            log.info('app started for testing');
-            callback(app);
-        } else {
-            var port = config.server.port;
-            server.listen(port, function (error) {
-                if (error) {
-                    throw error;
-                }
-                log.info('app server listening on port', port);
-                log.info('app status', config.status);
-            });
-        }
-    });
+        log.info('app server listening on port', port);
+        log.info('app status', config.status);
+      });
+    }
+  });
 };
--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ chai.use(dirtyChai);
 var expect = chai.expect;
 
 describe('Just adding a basic test example', function () {
-    describe('2 plus 2', function () {
-        it('should be 4', function () {
-            expect(2 + 2).to.be.equal(4);
-        })
+  describe('2 plus 2', function () {
+    it('should be 4', function () {
+      expect(2 + 2).to.be.equal(4);
     })
+  })
 });
